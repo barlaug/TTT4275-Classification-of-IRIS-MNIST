0add1953f9ddf7289916d260da78f984a7d2fda3
Eirik Runde Barlaug <barlaug@hotmail.no>
2022-04-02 12:34:59 +0200

    test

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,2 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from mnist import MNIST #NB: må installere i riktig mappe ved å skrive "pip install python-mnist" i terminal/command prompt
+
+#load data
+mndata = MNIST('./dir_with_mnist_data_files')
+images, labels = mndata.load_training()
 
 
