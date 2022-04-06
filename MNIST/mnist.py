15abbad7d07e480a3f20944d6ed27ff426b757ca
Eirik Runde Barlaug <barlaug@hotmail.no>
2022-04-06 15:57:32 +0200

    øl nå

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -1,5 +1,6 @@
+from tkinter import N
 import numpy as np
-from time import time
+import time
 from matplotlib import pyplot as plt
 from keras.datasets import mnist #!NB: pip install tensorflow, pip install keras -> Skriv i readme.
 from sklearn.neighbors import KNeighborsClassifier # NB: pip install sklearn
@@ -14,8 +15,7 @@ from sklearn.cluster import KMeans
 # BEGIN Utilities
 
 def flatten_split_data(x_train, y_train, x_test, y_test, n_chunks):
-    """Reshapes datasets to 2D-arrays and splits them into n chunks
-
+    """Reshapes datasets to 2D-arrays and splits them into n chunks.
     Params:
         x_train, y_train: np.ndarray
             Training dataset
@@ -26,7 +26,7 @@ def flatten_split_data(x_train, y_train, x_test, y_test, n_chunks):
     Returns:
         Reshaped arrays
     """
-    # Flatten 2D images into 1D arrays
+    # Flatten 2D data vectors/images into 1D arrays
     nsamples_train, nx_train, ny_train = x_train.shape
     x_train = x_train.reshape((nsamples_train,nx_train*ny_train))
     nsamples_test, nx_test, ny_test = x_test.shape
@@ -40,10 +40,8 @@ def flatten_split_data(x_train, y_train, x_test, y_test, n_chunks):
 
 
 def knn_predictions(x_train, y_train, x_test, k_neighbours):
-    """
-    Creates kNN classifier, fits it to training data and 
-    classifies the test data based of of the training samples
-
+    """Creates kNN classifier, fits it to training data and classifies 
+    the test data based of of the training samples.
     Params:
         x_train, y_train: np.ndarray
             Training dataset
@@ -53,18 +51,21 @@ def knn_predictions(x_train, y_train, x_test, k_neighbours):
             Number of neighbours in the kNN classifier
     Returns: 
         y_pred: np.ndarray
-            Predicted values for the test set
+            Predicted values for the test set'
+        fit_time: float
+            Time used to fit training data
     """
-    neigh = KNeighborsClassifier(n_neighbors=k_neighbours) # distance = Euclidean and n_jobs = 1 by default
+    neigh = KNeighborsClassifier(n_neighbors=k_neighbours, n_jobs=-1, metric='euclidean') # distance = Euclidean and n_jobs = 1 by default
+    tstart = time.time()
     neigh.fit(x_train, y_train) 
-    return neigh.predict(x_test)
+    fit_time = time.time() - tstart
+    print(fit_time)
+    return neigh.predict(x_test), fit_time
 
 
 def sort_predictions_knn(y_pred, y_test):
-    """
-    Finds the wrong and correct predictions in y_pred and
-    returns arrays with the corresponding index
-
+    """Finds the wrong and correct predictions in y_pred and returns 
+    arrays with the corresponding index.
     Params:
         y_pred: np.ndarray
             The values predicted by the classifier for the test set
@@ -85,10 +86,8 @@ def sort_predictions_knn(y_pred, y_test):
 
 
 def display_predictions(failed_indexes, correct_indexes, x_test, y_test, n):
-    """
-    Displays the first n correct or incorrect predictions that the 
-    classifier made
-
+    """Displays the first n correct or incorrect predictions that the 
+       classifier made.
     Params:
         failed_indexes: list
             indexes where the prediction was incorrect
@@ -118,7 +117,7 @@ def display_predictions(failed_indexes, correct_indexes, x_test, y_test, n):
     else: return
 
 def sort_data(data, labels):
-    """Sorts all members of x_train into the classes from y_train 
+    """Sorts all members of x_train into the classes from y_train.
     Params:
         data: np.ndarray
             Feature vectors 
@@ -139,7 +138,7 @@ def sort_data(data, labels):
 
 def cluster(data, labels, M):
     """ Clusters each class into M clusters with Kmeans(), each cluster 
-        center acts as a template vector for the given class in returned dict 
+        center acts as a template vector for the given class in the returned dict 
     Params:
         data: np.ndarray
             Feature vectors 
@@ -148,20 +147,30 @@ def cluster(data, labels, M):
         M: int
             Number of clusters
     Returns:
-        data_clustered: dict
-            Dict with new template vectors. Key = class,
-            value = 64 cluster centers for that class 
+        x_train_small: np.ndarray
+            ...
+        y_train_small: np.ndarray
+            ...
     """
     data_sorted = sort_data(data, labels)
-    n_classes = len(np.unique(labels)) 
-    # Init dict as {0: [], 1: [], ...} with n_classes empty arrays as values
-    data_clustered = {k: [] for k in range(n_classes)}
-    for i_class, members in data_sorted:
-        # Fit members to class
+    # Vectors for extending the dict into 2D training vectors
+    x_train_modified, y_train_modified = [], [] 
+    for class_i, members in data_sorted.items():
+        # Fit members to class with M clusters
         kmeans = KMeans(n_clusters=M).fit(members)
-        # Insert cluster centers as new members to class i_class
-        data_clustered[i_class].append(kmeans.cluster_centers_)
-    return data_clustered
+        # Insert array of cluster centers as new members to class i_class
+        x_train_modified.extend(kmeans.cluster_centers_)
+        # Append M elements of the class to make x and y have equal lengths
+        y_train_modified.extend(np.repeat(class_i, M))
+
+    # Convert to np.ndarray for passing to kNN classifier.
+    # Because means (cluster centroids) have lots of decimals, we 
+    # need .astype(int) to make arrays sparse, i.e. very small vals->0.
+    # If not then clustering won't be more effective than "normal" kNN... 
+    x_train_modified = np.array(x_train_modified).astype(int)
+    y_train_modified = np.array(y_train_modified).astype(int)
+
+    return x_train_modified, y_train_modified
 
 # END utilities
 
@@ -175,7 +184,7 @@ if __name__ == '__main__':
     n_chuncks = 10
     x_train, y_train, x_test, y_test = flatten_split_data(x_train, y_train, x_test, y_test, n_chuncks)
     # Choose which data chunck to use, 0 is fine - Change it for slightly different results
-    chunk = 0
+    chunk = 3
     x_train, y_train, x_test, y_test = x_train[chunk], y_train[chunk], x_test[chunk], y_test[chunk]
 
     # # BEGIN Task 1A
@@ -202,7 +211,29 @@ if __name__ == '__main__':
     # # Show some (n_corrects) correctly classified pictures
     # display_predictions([], correct_indexes, x_test, y_test, n_corrects)
     # # END Task 1C
-    sort_data(x_train, y_train)
+
+    # BEGIN Task 2A
+    d, x_train_clustr, y_train_clustr = cluster(x_train, y_train, M=64)
+    # Prove correct shape = (64, 28x28)
+    # for i in range(9):
+    #     print(str(d[i].shape))
+    # print(x)
+    # print("x_train:", str(x_train.shape))
+    # print("y_train:", str(y_train.shape))
+    # print("x_train_c:", str(x_train_clustr.shape))
+    # print("y_train_c:", str(y_train_clustr.shape))
+    k_neighs = 3
+    y_pred_clustr, t2 = knn_predictions(x_train_clustr, y_train_clustr, x_test, k_neighs)
+    y_pred, t1 = knn_predictions(x_train, y_train, x_test, k_neighs)
+    print(f'Fit times:\nWithout clustering: {t1}\nWith clustering: {t2}')
+    for i in range(500, 600):
+        print(x_train_clustr[i])
+    cm = confusion_matrix(y_test, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+    print(classification_report(y_test, y_pred_clustr)) 
+    # END Task 2A
 
 
 
