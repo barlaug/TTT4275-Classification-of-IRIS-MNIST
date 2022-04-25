04cfb0061cfc20d549687d8dc4c90f87d91a0bcf
Edvard Vedeler <edvedeler@gmail.com>
2022-04-25 19:35:07 +0200

    Classifier and training seemingly working as expected. Some issues with the testing/testing_set

--- Classification Iris/Iris_TTT4275/iris3.py
+++ Classification Iris/Iris_TTT4275/iris3.py
@@ -46,29 +46,29 @@ def extract_sets(data, targets, train_set_size, num_classes):
     test_set_size = N - train_set_size
     train_samples = np.zeros((num_classes*train_set_size,int(len(data[0]))+1))
     test_samples = np.zeros((num_classes*test_set_size,int(len(data[0]))+1))
-    t_train = np.zeros((num_classes, num_classes*train_set_size)) #3x90
+    y_true = np.zeros((num_classes*test_set_size))
+    t_train = np.zeros((num_classes*train_set_size, num_classes))
+    t_vec = np.zeros((int(len(targets)), num_classes)) 
+    
 
-    t_vec = np.zeros((num_classes, int(len(data)))) 
-    print(targets.shape)
-
-    for j in range(int(len(data))):
-        t_vec[targets[j]] = 1
+    for i in range(int(len(targets))):
+        t_vec[i][targets[i]] = 1
 
     for i in range(num_classes):
-        temp = data[(N*i):((i+1)*N)]
-        target_temp = targets[(N*i):((i+1)*N)]
-        print(target_temp.shape)
+        class_temp = data[(N*i):((i+1)*N)]
+        target_temp = t_vec[(N*i):((i+1)*N)]
+        y_temp = targets[(N*i):((i+1)*N)]
+
         # Add the ones in every x[4]
-        class_number = np.ones((temp.shape[0],1))
-        temp = np.hstack((temp, class_number))
-        print(temp.shape)
-        train_samples[(train_set_size*i):((i+1)*train_set_size), :] = temp[ :train_set_size,:]
-        test_samples[(test_set_size*i):((i+1)*test_set_size), :] = temp[train_set_size:,:]
-        t_train[(train_set_size*i):((i+1)*train_set_size), :] = target_temp[ :train_set_size,:]
-        
-    print(t_vec)
-    print(t_train)
-    return train_samples, test_samples, t_train
+        class_number = np.ones((class_temp.shape[0],1))
+        class_temp = np.hstack((class_temp, class_number))
+
+        train_samples[(train_set_size*i):((i+1)*train_set_size), :] = class_temp[ :train_set_size,:]
+        test_samples[(test_set_size*i):((i+1)*test_set_size), :] = class_temp[train_set_size:,:]
+        t_train[(train_set_size*i):((i+1)*train_set_size), :] = target_temp[ :train_set_size, :]
+        y_true[(test_set_size*i):((i+1)*test_set_size)] = y_temp[train_set_size:]
+
+    return train_samples, test_samples, t_train, y_true
 
 
 def sigmoid(z_k):
@@ -120,50 +120,69 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations):
     """
     num_classes = Classes
     num_features = int(len(train_samples[0])) #5
-    print(num_features)
-    print(train_samples.shape)
+
     W = np.zeros((num_classes,num_features))
-    print(W.shape)
-    t_k = np.zeros((num_classes,1)) #np.array([[1 ,0 ,0] ,[0 ,1 ,0] ,[0 ,0 ,1]])
-    #t_k = np.array([[1 ,0 ,0] ,[0 ,1 ,0] ,[0 ,0 ,1]])
-    g_k = np.zeros((num_classes)) # The input values we want to minimise MSE for
-    g_k [0] = 1 # always start with 1
+
+    t_k = np.zeros((num_classes,1))
+    g_k = np.zeros((num_classes,1)) # The input values we want to minimise MSE for
+    g_k[0] = 1 # always start with 1
     MSEset = []
     for i in range(num_iterations):
-        MSEset = 0
+        MSE = 0
         grad_W_MSE = 0
         for k in range(int(len(train_samples))):
-            x_k = train_samples[k]
-            print(x_k)
-            print(W)
+            x_k = np.reshape(train_samples[k], (1, 5))
             z_k = np.matmul(W, x_k.T)
-            g_k = sigmoid(z_k)
-            
+            g_k = np.reshape(sigmoid(z_k), (1, 3))
 
-            t_k = train_samples[k]
 
-            print(g_k)
-            print(t_k)
+            t_k = np.reshape(train_targets[k], (1, 3))
 
-            grad_g_MSE = g_k - t_k #.T?
-
+            grad_g_MSE = g_k - t_k
             grad_z_gk = np.multiply(g_k,(1 - g_k)) #element wise
-            grad_W_z = x_k #.T?
+            grad_W_z = x_k
 
-            grad = np.multiply(np.multiply((g_k - t_k), g_k),(1 - g_k))
-            grad2 = np.multiply(grad_g_MSE,grad_z_gk)
-            print(grad.shape)
-            print(grad2.shape)
-            print(grad_W_z.shape)
-            grad_W_MSE = grad_W_MSE + np.matmul(grad, grad_W_z) #np.multiply(grad_g_MSE,grad_z_gk)
-
+            grad = np.multiply(grad_g_MSE,grad_z_gk).T
             
+            grad_W_MSE = grad_W_MSE + np.matmul(grad, grad_W_z)
+
             MSE = 0.5*np.multiply((g_k - t_k).T, (g_k - t_k))
         
         MSEset.append(MSE)
-        W = W - alpha*grad_W_MSE #.T
+        W = W - alpha*grad_W_MSE
     MSEarray = np.array(MSEset)
-    return W, MSEarray
+    return W #, MSEarray
+
+def discriminant_classifier(W, test_samples, y_true):
+    """Decision rule and discriminant classifier. Equations (6)-(7) in compendium implemented
+    Params:
+        W: np.array
+            CxD+1 classifier
+        test_samples: np.ndarray
+
+    Returns:
+        g: 
+
+    """
+    x_test = np.zeros((1, len(test_samples[0])))
+    g_k = np.zeros((num_classes,1))
+    y_pred = np.zeros(len(test_samples))
+    
+    for i in range(len(test_samples)):
+        x_test = np.reshape(test_samples[i,:], (1, 5))
+        g_i = np.matmul(W, x_test.T)
+        print(g_i)
+        g_j = int(np.argmax(g_i))
+        print(g_j)
+        y_pred[i] = g_j
+    print(y_pred)
+
+
+    return y_pred
+
+
+
+
 
 
 def display_CM_Error(y_pred, y_true):
@@ -181,9 +200,10 @@ def display_CM_Error(y_pred, y_true):
     print('Classification report:')
     print(classification_report(y_true, y_pred)) # Accuracy is given in report
 
-    #accuracy = zero_one_score(y_test, y_pred)
-    #error_rate = 1 - accuracy
-    #print(f"Error rate: {error_rate}%")
+def print_error_rate(y_true, y_pred):
+    accuracy = zero_one_loss(y_true, y_pred)
+    error_rate = (1 - accuracy)*100
+    print(f"Error rate: {error_rate}%")
 
 
 if __name__ == '__main__':
@@ -191,15 +211,19 @@ if __name__ == '__main__':
     iris_data, iris_legends, iris_targets = load_data()
     #alphas = [0.5, 0.25, 0.1, 0.05, 0.005]
     alphas = [0.05]
-    iterations = 5000
+    iterations = 1000
     train_set_size = 30
-    train_set, test_set, t_train = extract_sets(iris_data, iris_targets, train_set_size, num_classes)
+    train_set, test_set, t_train, y_true = extract_sets(iris_data, iris_targets, train_set_size, num_classes)
     for alpha in alphas:
         print("30 train, 20 test")
         W = train_lin_model(train_set[:,:], t_train, alpha, iterations) #[:,:]? 
-        pred_train = get_prediction(W, train_set)
-        pred_test =  get_prediction(W, test_set)
+        #pred_train = get_prediction(W, train_set)
+        #pred_test =  get_prediction(W, test_set)
         print("Training")
-        get_prediction(W, test_set)
-        #display_CM_Error(y_pred, y_true)
+        print(W)
+        #get_prediction(W, test_set)
+        y_pred = discriminant_classifier(W, test_set, y_true)
+        display_CM_Error(y_pred, y_true)
+        print_error_rate(y_true, y_pred)
+        
         
