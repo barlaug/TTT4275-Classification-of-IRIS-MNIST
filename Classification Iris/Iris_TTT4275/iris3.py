ef53ba35fb6c59f4e48838252dcefff22864dbda
Oscar Meisal <oscarmeisal@gmail.com>
2022-04-25 13:29:20 +0200

    Debugging dimensions

--- Classification Iris/Iris_TTT4275/iris3.py
+++ Classification Iris/Iris_TTT4275/iris3.py
@@ -5,7 +5,7 @@ import numpy as np
 import copy
 
 
-import seaborn as sns
+#import seaborn as sns
 
 from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, zero_one_loss
 from sklearn.datasets import load_iris
@@ -46,18 +46,29 @@ def extract_sets(data, targets, train_set_size, num_classes):
     test_set_size = N - train_set_size
     train_samples = np.zeros((num_classes*train_set_size,int(len(data[0]))+1))
     test_samples = np.zeros((num_classes*test_set_size,int(len(data[0]))+1))
+    t_train = np.zeros((num_classes, num_classes*train_set_size)) #3x90
+
+    t_vec = np.zeros((num_classes, int(len(data)))) 
+    print(targets.shape)
+
+    for j in range(int(len(data))):
+        t_vec[targets[j]] = 1
 
     for i in range(num_classes):
         temp = data[(N*i):((i+1)*N)]
+        target_temp = targets[(N*i):((i+1)*N)]
+        print(target_temp.shape)
         # Add the ones in every x[4]
         class_number = np.ones((temp.shape[0],1))
         temp = np.hstack((temp, class_number))
         print(temp.shape)
         train_samples[(train_set_size*i):((i+1)*train_set_size), :] = temp[ :train_set_size,:]
         test_samples[(test_set_size*i):((i+1)*test_set_size), :] = temp[train_set_size:,:]
+        t_train[(train_set_size*i):((i+1)*train_set_size), :] = target_temp[ :train_set_size,:]
         
-
-    return train_samples, test_samples
+    print(t_vec)
+    print(t_train)
+    return train_samples, test_samples, t_train
 
 
 def sigmoid(z_k):
@@ -92,7 +103,7 @@ def get_prediction(W, x):
     return prediction_vec
 
 
-def train_lin_model(train_samples, alpha, num_iterations):
+def train_lin_model(train_samples, train_targets, alpha, num_iterations):
     """Trains the linear model. Equations (19)-(23) in compendium implemented
     Params:
         train_samples: np.array
@@ -111,33 +122,38 @@ def train_lin_model(train_samples, alpha, num_iterations):
     num_features = int(len(train_samples[0])) #5
     print(num_features)
     print(train_samples.shape)
-    classes, samples, features = train_samples.shape
-    print(classes)
-    print(samples)
-    print(features)
     W = np.zeros((num_classes,num_features))
-    t_k = np.array([[1 ,0 ,0] ,[0 ,1 ,0] ,[0 ,0 ,1]])
+    print(W.shape)
+    t_k = np.zeros((num_classes,1)) #np.array([[1 ,0 ,0] ,[0 ,1 ,0] ,[0 ,0 ,1]])
+    #t_k = np.array([[1 ,0 ,0] ,[0 ,1 ,0] ,[0 ,0 ,1]])
     g_k = np.zeros((num_classes)) # The input values we want to minimise MSE for
     g_k [0] = 1 # always start with 1
     MSEset = []
     for i in range(num_iterations):
         MSEset = 0
         grad_W_MSE = 0
-        for x_k in train_samples:
+        for k in range(int(len(train_samples))):
+            x_k = train_samples[k]
+            print(x_k)
+            print(W)
             z_k = np.matmul(W, x_k.T)
             g_k = sigmoid(z_k)
+            
+
+            t_k = train_samples[k]
+
             print(g_k)
+            print(t_k)
 
             grad_g_MSE = g_k - t_k #.T?
-            print(grad_g_MSE)
 
             grad_z_gk = np.multiply(g_k,(1 - g_k)) #element wise
-            print(grad_z_gk)
             grad_W_z = x_k #.T?
-            print(grad_W_z)
 
             grad = np.multiply(np.multiply((g_k - t_k), g_k),(1 - g_k))
+            grad2 = np.multiply(grad_g_MSE,grad_z_gk)
             print(grad.shape)
+            print(grad2.shape)
             print(grad_W_z.shape)
             grad_W_MSE = grad_W_MSE + np.matmul(grad, grad_W_z) #np.multiply(grad_g_MSE,grad_z_gk)
 
@@ -177,10 +193,10 @@ if __name__ == '__main__':
     alphas = [0.05]
     iterations = 5000
     train_set_size = 30
-    train_set, test_set = extract_sets(iris_data, iris_targets, train_set_size, num_classes)
+    train_set, test_set, t_train = extract_sets(iris_data, iris_targets, train_set_size, num_classes)
     for alpha in alphas:
         print("30 train, 20 test")
-        W = train_lin_model(train_set[:,:], alpha, iterations) #[:,:]? 
+        W = train_lin_model(train_set[:,:], t_train, alpha, iterations) #[:,:]? 
         pred_train = get_prediction(W, train_set)
         pred_test =  get_prediction(W, test_set)
         print("Training")
