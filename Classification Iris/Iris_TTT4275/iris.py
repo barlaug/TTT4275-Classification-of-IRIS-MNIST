2fe2e7350eac41d8fd808994ff9e6abd8eb2ae70
Edvard Vedeler <edvedeler@gmail.com>
2022-04-27 17:04:06 +0200

    Split code into two files. iris.py and plot.py. Last changes

--- Classification Iris/Iris_TTT4275/iris.py
+++ Classification Iris/Iris_TTT4275/iris.py
@@ -1,11 +1,11 @@
 # Importing the modules
 from distutils.log import error
-import matplotlib.pyplot as plt
 import numpy as np
 
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, zero_one_loss
+from sklearn.metrics import zero_one_loss
 from sklearn.datasets import load_iris
 
+import plot
 
 def load_data():
     """Loads iris with sklearn
@@ -28,9 +28,11 @@ def load_data():
 def extract_sets(data, targets, train_set_size, num_classes):
     """Splits data into training set and testing set
     Params:
-       data: np.ndarray
+        data: np.ndarray
             Full iris dataset, with all samples for every class
-       train_set_size: int
+        targets: np.ndarray
+            Array of target
+        train_set_size: int
             Desired size of training set
         num_classes: int
             Desired size of training set
@@ -89,7 +91,7 @@ def sigmoid(z_k):
 
 
 def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_classes):
-    """Trains the linear model. Equations (19)-(23) in compendium implemented
+    """Trains the linear model. Equations (19) and (21)-(23) in compendium implemented
     Params:
         train_samples: np.array
             The training samples
@@ -102,14 +104,11 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_cla
     Returns:
         W: np.array
             Cx(D+1) classifier
-        MSEarray: np.array
-            Array containing MSEs from the calculations. For plotting
+        W_set, iter_set, MSE_set: array
+            Arrays containing samples of W, iteration number and MSE, respectively. For plotting
     """
     num_features = int(len(train_samples[0]))
     W = np.zeros((num_classes,num_features))
-    x_k = np.zeros((1, num_features))
-    t_k = np.zeros((num_classes,1))
-    z_k = np.zeros((num_classes,1))
     g_k = np.zeros((num_classes,1)) # The input values we want to minimise MSE for
     g_k[0] = 1 # Start with 1
     MSE_set = []
@@ -125,7 +124,7 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_cla
             t_k = np.reshape(train_targets[k], (num_classes, 1))
 
             grad_g_MSE = g_k - t_k
-            grad_z_gk = np.multiply(g_k,(1 - g_k)) #element wise
+            grad_z_gk = np.multiply(g_k,(1 - g_k))
             grad_W_z = x_k
             grad = np.multiply(grad_g_MSE,grad_z_gk)
 
@@ -134,7 +133,8 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_cla
             MSE = MSE + 0.5*np.multiply((g_k - t_k).T, (g_k - t_k))
         W = W - alpha*grad_W_MSE
         #Sampling every 50 iteration for plotting
-        """Comment this in if you want to plot Error rate vs iterations 
+        #Comment this in if you want to plot Error rate vs iterations 
+        """
         if not i % 20:
             W_set.append(W)
             MSE_set.append(MSE)
@@ -163,82 +163,6 @@ def discriminant_classifier(W, test_samples):
         g_j = int(np.argmax(g_i))
         y_pred[i] = g_j
     return y_pred
-
-
-def display_ER_MSE_iterations(x, targets, train_set_size, num_classes):
-    x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(x, targets, train_set_size, num_classes)
-    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-    #Values for which we want to test
-    alphas = [0.0005, 0.001, 0.005, 0.05]#, 0.001, 0.005, 0.01, 0.05]
-    iterations = 3000
-    for alpha in alphas:
-        print("Alpha: " + str(alpha))
-        error_rate_set = []
-        alpha_label = "alpha="+ str(alpha)
-        W1, W_set, iter_set, MSE_set = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
-        for W in W_set:
-            y_pred = discriminant_classifier(W, x_test)
-            error_rate = get_error_rate(y_test, y_pred)
-            error_rate_set.append(error_rate)
-
-        error_rate_vec = np.array(error_rate_set)
-        iteration_vec = np.array(iter_set)
-        MSE_vec = np.array(MSE_set)
-        ax1.plot(iteration_vec, error_rate_vec, label=alpha_label)
-        ax1.legend(fontsize=12)
-        ax2.plot(iteration_vec, MSE_vec[:,0,0], label=alpha_label)
-        ax2.legend(fontsize=12)
-    ax1.set_ylabel("Error rate", fontsize=14)
-    ax2.set_ylabel("MSE", fontsize=14)
-    #ax1.set_xlabel("Iterations", fontsize=14)
-    ax2.set_xlabel("Iterations", fontsize=14)
-    ax1.set_title("Error rate vs Iterations for different alpha", fontsize=16)
-    ax2.set_title("MSE vs Iterations for different alpha", fontsize=16)
-    
-    print('Plotting ER vs. iterations...')
-    plt.savefig('error_rate_MSE_vs_iterations2.eps', format='eps')
-    plt.show()
-
-
-def display_CM_Error(y_pred, y_true, legends):
-    """Displays confusion matrix and error rate
-       for deviation between predicted and true labels
-    Params:
-        y_pred, y_true: np.ndarray
-            Predicted and true labels/classes
-    """
-    error_rate = (zero_one_loss(y_true, y_pred))*100
-    print(f"Error rate: {error_rate}%")
-    cm = confusion_matrix(y_true, y_pred)
-    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=legends)
-    disp.plot()
-    print('Displaying confusion matrix...')
-    plt.show()
-
-
-def plot_histogram(x, num_classes, labels):
-    """Displays histogram for the four features from every class
-    Params:
-        x: np.ndarray
-            Complete iris dataset
-        num_classes: int
-            Number of classes
-    """
-    features = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
-    N = int(len(x)/num_classes)
-    for i in range(len(features)):
-        n_bins = np.arange(0, 8,1/2)
-        plt.subplot(2, 2, i+1)
-        plt.hist(x[:N,i], alpha=0.5, label=labels[0])
-        plt.hist(x[N:2*N,i], alpha=0.5, label=labels[0])
-        plt.hist(x[2*N:len(x),i], alpha=0.5, label=labels[0])
-        plt.legend()
-        plt.xlabel(features[i])
-        plt.ylabel("Count")
-        plt.xlabel("%s [cm]" % features[i])
-    #plt.savefig('histogram.eps', format='eps')
-    print("Plotting histogram")
-    plt.show()
 
 
 def get_error_rate(y_true, y_pred):
@@ -274,45 +198,41 @@ if __name__ == '__main__':
     train_set_size = 30
     test_set_size = 20 #needed for task 1d)
 
-    #plot_histogram(iris_data, num_classes, iris_legends)
-    #display_ER_MSE_iterations(iris_data, iris_targets, train_set_size, num_classes)
+    #plot.plot_histogram(iris_data, num_classes, iris_legends)
+    #plot.display_ER_MSE_iterations(iris_data, iris_targets, train_set_size, num_classes)
 
     #TASK 1
     print("\n-------------------------------------------------------------\n")
     alpha = 0.006
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
         iris_data, iris_targets, train_set_size, num_classes)
-    print("Task 1a)-c)")
-    print("30 training samples, 20 test samples from each class")
+    print("Task 1a)-c)\n 30 training samples, 20 test samples from each class")
     W1 = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
     print("Confusion matrix and error rate for the testing set:")
     y_pred_test = discriminant_classifier(W1, x_test)
-    display_CM_Error(y_pred_test, y_test, iris_legends)
+    plot.display_CM_Error(y_pred_test, y_test, iris_legends)
     
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W1, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends)
-
+    plot.display_CM_Error(y_pred_train, y_train, iris_legends)
 
     print("\n-------------------------------------------------------------\n")
     #TASK 1d)
     x_test, x_train, t_test, t_train, y_test, y_train = extract_sets(
         iris_data, iris_targets, test_set_size, num_classes)
-    print("Task 1d)")
-    print("30 last samples as training samples, 20 first samples as test samples")
+    print("Task 1d)\n 30 last samples as training samples, 20 first samples as test samples")
     W2 = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
     print("Confusion matrix and error rate for the testing set:")
     y_pred_test = discriminant_classifier(W2, x_test)
-    display_CM_Error(y_pred_test, y_test, iris_legends)
+    plot.display_CM_Error(y_pred_test, y_test, iris_legends)
     
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W2, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends)
+    plot.display_CM_Error(y_pred_train, y_train, iris_legends)
     
     print("\n-------------------------------------------------------------\n")
     #TASK 2
-    print("Task 2a)")
-    print("Removing feature(s) with the most overlap:")
+    print("Task 2a)\n Removing feature(s) with the most overlap:")
     print("Removing Sepal width:")
     iris_data_1 = remove_feature(iris_data,1)
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
@@ -321,12 +241,11 @@ if __name__ == '__main__':
     W3 = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
     print("Confusion matrix and error rate for the testing set:")
     y_pred_test = discriminant_classifier(W3, x_test)
-    display_CM_Error(y_pred_test, y_test, iris_legends)
+    plot.display_CM_Error(y_pred_test, y_test, iris_legends)
     
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W3, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends)
-    
+    plot.display_CM_Error(y_pred_train, y_train, iris_legends)
     #"""
     print("Removing Sepal length:")
     iris_data_2 = remove_feature(iris_data_1,0)
@@ -336,10 +255,10 @@ if __name__ == '__main__':
     W4 = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
     print("Confusion matrix and error rate for the testing set:")
     y_pred_test = discriminant_classifier(W4, x_test)
-    display_CM_Error(y_pred_test, y_test, iris_legends)
+    plot.display_CM_Error(y_pred_test, y_test, iris_legends)
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W4, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends)
+    plot.display_CM_Error(y_pred_train, y_train, iris_legends)
 
     print("Removing Petal width:")
     iris_data_3 = remove_feature(iris_data_2,1) #Hvilken index blir riktig å fjerne her?
@@ -349,13 +268,8 @@ if __name__ == '__main__':
     W5 = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
     print("Confusion matrix and error rate for the testing set:")
     y_pred_test = discriminant_classifier(W5, x_test)
-    display_CM_Error(y_pred_test, y_test, iris_legends)
+    plot.display_CM_Error(y_pred_test, y_test, iris_legends)
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W5, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends)
-    #"""
-
-    print("\n-------------------------------------------------------------\n")
-
-        
-        
+    plot.display_CM_Error(y_pred_train, y_train, iris_legends)
+    #"""        
---
+++ Classification Iris/Iris_TTT4275/plot.py
@@ -0,0 +1,93 @@
+from distutils.log import error
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, zero_one_loss
+
+import iris
+
+
+def display_ER_MSE_iterations(x, targets, train_set_size, num_classes):
+    """Displays plots for Error rate vs iterations and MSE vs iterations, 
+        respectively, for different alphas
+    Params:
+        x: np.ndarray
+            Complete iris dataset
+        targets: np.ndarray
+            Complete iris dataset
+        x: np.ndarray
+            Complete iris dataset
+        num_classes: int
+            Number of classes
+    """
+    x_train, x_test, t_train, t_test, y_train, y_test = iris.extract_sets(x, targets, train_set_size, num_classes)
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    #Values for which we want to test
+    alphas = [0.0005, 0.001, 0.005, 0.05]#, 0.001, 0.005, 0.01, 0.05]
+    iterations = 3000
+    for alpha in alphas:
+        print("Alpha: " + str(alpha))
+        error_rate_set = []
+        alpha_label = "alpha="+ str(alpha)
+        W1, W_set, iter_set, MSE_set = iris.train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
+        for W in W_set:
+            y_pred = iris.discriminant_classifier(W, x_test)
+            error_rate = iris.get_error_rate(y_test, y_pred)
+            error_rate_set.append(error_rate)
+        error_rate_vec = np.array(error_rate_set)
+        iteration_vec = np.array(iter_set)
+        MSE_vec = np.array(MSE_set)
+        #Plotting:
+        ax1.plot(iteration_vec, error_rate_vec, label=alpha_label)
+        ax1.legend(fontsize=12)
+        ax2.plot(iteration_vec, MSE_vec[:,0,0], label=alpha_label)
+        ax2.legend(fontsize=12)
+    ax1.set_ylabel("Error rate", fontsize=14)
+    ax2.set_ylabel("MSE", fontsize=14)
+    ax2.set_xlabel("Iterations", fontsize=14)
+    ax1.set_title("Error rate vs Iterations for different alpha", fontsize=16)
+    ax2.set_title("MSE vs Iterations for different alpha", fontsize=16)
+    print('Plotting ER and MSE vs. iterations...\n')
+    #plt.savefig('error_rate_MSE_vs_iterations2.eps', format='eps')
+    plt.show()
+
+
+def display_CM_Error(y_pred, y_true, legends):
+    """Displays confusion matrix and error rate
+       for deviation between predicted and true labels
+    Params:
+        y_pred, y_true: np.ndarray
+            Predicted and true labels/classes
+    """
+    error_rate = (zero_one_loss(y_true, y_pred))*100
+    print(f"Error rate: {error_rate}%")
+    cm = confusion_matrix(y_true, y_pred)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=legends)
+    disp.plot()
+    print('Displaying confusion matrix...\n')
+    plt.show()
+
+
+def plot_histogram(x, num_classes, labels):
+    """Displays histogram for the four features from every class
+    Params:
+        x: np.ndarray
+            Complete iris dataset
+        num_classes: int
+            Number of classes
+    """
+    features = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
+    N = int(len(x)/num_classes)
+    for i in range(len(features)):
+        n_bins = np.arange(0, 8,1/2)
+        plt.subplot(2, 2, i+1)
+        plt.hist(x[:N,i], alpha=0.5, label=labels[0])
+        plt.hist(x[N:2*N,i], alpha=0.5, label=labels[0])
+        plt.hist(x[2*N:len(x),i], alpha=0.5, label=labels[0])
+        plt.legend()
+        plt.xlabel(features[i])
+        plt.ylabel("Count")
+        plt.xlabel("%s [cm]" % features[i])
+    #plt.savefig('histogram.eps', format='eps')
+    print("Plotting histogram...\n")
+    plt.show()
