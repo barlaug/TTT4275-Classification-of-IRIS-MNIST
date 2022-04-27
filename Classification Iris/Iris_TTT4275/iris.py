9ae9ea3b1e9d7674904ac70a54f69d6965b0d624
Edvard Vedeler <edvedeler@gmail.com>
2022-04-27 16:39:21 +0200

    All plots up and running. Task complete. Before deviding into two files

--- Classification Iris/Iris_TTT4275/iris.py
+++ Classification Iris/Iris_TTT4275/iris.py
@@ -1,11 +1,9 @@
 # Importing the modules
 from distutils.log import error
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import copy
 
-from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay, zero_one_loss
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, zero_one_loss
 from sklearn.datasets import load_iris
 
 
@@ -45,7 +43,6 @@ def extract_sets(data, targets, train_set_size, num_classes):
                 testset respectively
     """
     N = int(len(data)/num_classes)
-
     test_set_size = N - train_set_size
 
     train_samples = np.zeros((num_classes*train_set_size,int(len(data[0]))+1))
@@ -115,7 +112,9 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_cla
     z_k = np.zeros((num_classes,1))
     g_k = np.zeros((num_classes,1)) # The input values we want to minimise MSE for
     g_k[0] = 1 # Start with 1
-    MSEset = []
+    MSE_set = []
+    W_set = []
+    iter_set = []
     for i in range(num_iterations):
         MSE = 0
         grad_W_MSE = 0
@@ -133,11 +132,15 @@ def train_lin_model(train_samples, train_targets, alpha, num_iterations, num_cla
             grad_W_MSE = grad_W_MSE + np.matmul(grad, grad_W_z)
 
             MSE = MSE + 0.5*np.multiply((g_k - t_k).T, (g_k - t_k))
-        
-        MSEset.append(MSE)
         W = W - alpha*grad_W_MSE
-    MSEarray = np.array(MSEset)
-    return W #, MSEarray
+        #Sampling every 50 iteration for plotting
+        """Comment this in if you want to plot Error rate vs iterations 
+        if not i % 20:
+            W_set.append(W)
+            MSE_set.append(MSE)
+            iter_set.append(i)
+    return W, W_set, iter_set, MSE_set"""
+    return W
 
 
 def discriminant_classifier(W, test_samples):
@@ -162,52 +165,43 @@ def discriminant_classifier(W, test_samples):
     return y_pred
 
 
-def tune_alpha(alphas, iterations, x, targets, train_set_size, num_classes):
+def display_ER_MSE_iterations(x, targets, train_set_size, num_classes):
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(x, targets, train_set_size, num_classes)
-    error_rate_set = []
-    #Eks 5 alphas mellom 0.0001 og 0.1og 15 forskjellige iterations melom 0 og 3500
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+    #Values for which we want to test
+    alphas = [0.0005, 0.001, 0.005, 0.05]#, 0.001, 0.005, 0.01, 0.05]
+    iterations = 3000
     for alpha in alphas:
-        for i in iterations:
-            W = train_lin_model(x_train[:,:], t_train, alpha, i, num_classes)
+        print("Alpha: " + str(alpha))
+        error_rate_set = []
+        alpha_label = "alpha="+ str(alpha)
+        W1, W_set, iter_set, MSE_set = train_lin_model(x_train[:,:], t_train, alpha, iterations, num_classes)
+        for W in W_set:
             y_pred = discriminant_classifier(W, x_test)
             error_rate = get_error_rate(y_test, y_pred)
             error_rate_set.append(error_rate)
 
-    error_rate_vec = np.array(error_rate_set)
-    alpha_vec = np.array(alphas)
-    plt.scatter(alpha_vec, error_rate_vec)
-    plt.ylabel("Error rate")
-    plt.xlabel("alpha")
-    plt.title("Error rate as a function of alpha")
-    #print("Alpha value:", best_alpha)
-    plt.show()
-
-
-def illusrate_alphe_iterations(alphas, iterations, x, targets, train_set_size, num_classes):
-    x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(x, targets, train_set_size, num_classes)
-    error_rate_set = []
-    for alpha in alphas:
-        for i in iterations:
-            W = train_lin_model(x_train[:,:], t_train, alpha, int(i), num_classes)
-            y_pred = discriminant_classifier(W, x_test)
-            error_rate = get_error_rate(y_test, y_pred)
-            error_rate_set.append(error_rate)
-    error_rate_vec = np.array(error_rate_set)
-    alpha_vec = np.array(alphas)
+        error_rate_vec = np.array(error_rate_set)
+        iteration_vec = np.array(iter_set)
+        MSE_vec = np.array(MSE_set)
+        ax1.plot(iteration_vec, error_rate_vec, label=alpha_label)
+        ax1.legend(fontsize=12)
+        ax2.plot(iteration_vec, MSE_vec[:,0,0], label=alpha_label)
+        ax2.legend(fontsize=12)
+    ax1.set_ylabel("Error rate", fontsize=14)
+    ax2.set_ylabel("MSE", fontsize=14)
+    #ax1.set_xlabel("Iterations", fontsize=14)
+    ax2.set_xlabel("Iterations", fontsize=14)
+    ax1.set_title("Error rate vs Iterations for different alpha", fontsize=16)
+    ax2.set_title("MSE vs Iterations for different alpha", fontsize=16)
     
-    for i in range(len(alphas)):
-        plt.scatter(error_rate_vec[i*int(iterations[i]):(i+1)*int(iterations[i])], iterations[i])
-        plt.ylabel("Error rate")
-        plt.xlabel("Iterations")
-        plt.title("Error rate as a function of iterations for different values of alpha")
-        #print("Alpha value:", best_alpha)
+    print('Plotting ER vs. iterations...')
+    plt.savefig('error_rate_MSE_vs_iterations2.eps', format='eps')
     plt.show()
 
-
- 
 
 def display_CM_Error(y_pred, y_true, legends):
-    """Displays confusion matrix, classification report/errors(optional) and error rate
+    """Displays confusion matrix and error rate
        for deviation between predicted and true labels
     Params:
         y_pred, y_true: np.ndarray
@@ -220,9 +214,31 @@ def display_CM_Error(y_pred, y_true, legends):
     disp.plot()
     print('Displaying confusion matrix...')
     plt.show()
-    #print('Classification report:')
-    #print(classification_report(y_true, y_pred)) # Accuracy is given in report
-    #Print error_rate
+
+
+def plot_histogram(x, num_classes, labels):
+    """Displays histogram for the four features from every class
+    Params:
+        x: np.ndarray
+            Complete iris dataset
+        num_classes: int
+            Number of classes
+    """
+    features = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
+    N = int(len(x)/num_classes)
+    for i in range(len(features)):
+        n_bins = np.arange(0, 8,1/2)
+        plt.subplot(2, 2, i+1)
+        plt.hist(x[:N,i], alpha=0.5, label=labels[0])
+        plt.hist(x[N:2*N,i], alpha=0.5, label=labels[0])
+        plt.hist(x[2*N:len(x),i], alpha=0.5, label=labels[0])
+        plt.legend()
+        plt.xlabel(features[i])
+        plt.ylabel("Count")
+        plt.xlabel("%s [cm]" % features[i])
+    #plt.savefig('histogram.eps', format='eps')
+    print("Plotting histogram")
+    plt.show()
 
 
 def get_error_rate(y_true, y_pred):
@@ -238,29 +254,16 @@ def get_error_rate(y_true, y_pred):
     return error_rate
 
 
-def plot_histogram(x, num_classes):
-    """Displays histogram for the four features from every class
+def remove_feature(x, feature_index):
+    """Removes feature
     Params:
         x: np.ndarray
             Complete iris dataset
-        num_classes: int
-            Number of classes
+        feature_index: int
+            index of feature to remove
+    Returns:
+        modified dataset: np.ndarray
     """
-    features = ["Sepal length", "Sepal width", "Petal length", "Petal width"]
-    N = int(len(x)/num_classes)
-    for i in range(len(features)):
-        n_bins = np.arange(0, 8,1/2)
-        plt.subplot(2, 2, i+1)
-        #colors={0:'r',1:'g',2:'b',3:'k'}
-        plt.hist([x[:N,i], x[N:2*N,i], x[2*N:len(x),i]], bins = n_bins) #, color = colors, ec = colors)
-        plt.xlabel(features[i])
-        plt.ylabel("Count")
-        plt.xlabel("%s [cm]" % features[i])
-    print("Plotting histogram")
-    plt.show()
-
-
-def remove_feature(x, feature_index):
     return np.delete(x, feature_index, axis=1)
 
 
@@ -271,17 +274,12 @@ if __name__ == '__main__':
     train_set_size = 30
     test_set_size = 20 #needed for task 1d)
 
+    #plot_histogram(iris_data, num_classes, iris_legends)
+    #display_ER_MSE_iterations(iris_data, iris_targets, train_set_size, num_classes)
+
     #TASK 1
-    plot_histogram(iris_data, num_classes)
-
-    #Eks 5 alphas mellom 0.0001 og 0.1og 15 forskjellige iterations melom 0 og 3500
-    alphas = [0.0005, 0.001, 0.005, 0.01, 0.05]
-    iter = np.linspace(0, 100, 10)
-    #tune_alpha(alphas, iris_data, iris_targets, train_set_size, iterations, num_classes)
-    illusrate_alphe_iterations(alphas, iter, iris_data, iris_targets, train_set_size, num_classes)
     print("\n-------------------------------------------------------------\n")
-    alpha = [0.006]
-
+    alpha = 0.006
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
         iris_data, iris_targets, train_set_size, num_classes)
     print("Task 1a)-c)")
@@ -293,7 +291,7 @@ if __name__ == '__main__':
     
     print("Confusion matrix and error rate for the training set:")
     y_pred_train = discriminant_classifier(W1, x_train)
-    display_CM_Error(y_pred_train, y_train, iris_legends) #Dimensions!
+    display_CM_Error(y_pred_train, y_train, iris_legends)
 
 
     print("\n-------------------------------------------------------------\n")
@@ -315,7 +313,6 @@ if __name__ == '__main__':
     #TASK 2
     print("Task 2a)")
     print("Removing feature(s) with the most overlap:")
-    #removing 1 feature. Removing index 1 (Sepal width)
     print("Removing Sepal width:")
     iris_data_1 = remove_feature(iris_data,1)
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
@@ -331,7 +328,6 @@ if __name__ == '__main__':
     display_CM_Error(y_pred_train, y_train, iris_legends)
     
     #"""
-    #removing 2 features. Removing index 1, 0 ():
     print("Removing Sepal length:")
     iris_data_2 = remove_feature(iris_data_1,0)
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
@@ -345,7 +341,6 @@ if __name__ == '__main__':
     y_pred_train = discriminant_classifier(W4, x_train)
     display_CM_Error(y_pred_train, y_train, iris_legends)
 
-    #removing 3 features. Removing 3 (1) ():
     print("Removing Petal width:")
     iris_data_3 = remove_feature(iris_data_2,1) #Hvilken index blir riktig å fjerne her?
     x_train, x_test, t_train, t_test, y_train, y_test = extract_sets(
